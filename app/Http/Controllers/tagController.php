efffe62ebae2e6d01ec7b459739c4652ef71a36a
mario ornelas <mariodom987_@hotmail.com>
2020-07-29 18:37:16 -0500

    abc Tag

--- app/Http/Controllers/tagController.php
+++ app/Http/Controllers/tagController.php
@@ -7,6 +7,7 @@ use App\tag;
 use App\Http\Requests;
 class tagController extends Controller
 {
+
     public function vista(){
         return view('agregarTag');
     }
@@ -18,5 +19,41 @@ class tagController extends Controller
         $tag -> save();
 
         return redirect('/create');
+    }
+
+    public function read(Request $request){
+        $tag = tag::all();
+        return view('listaTag',['lista'=>$tag]);
+    }
+
+    public function edit(tag $tag)
+    {
+        return view('actualizarTag', [
+            'tag' => $tag
+        ]);
+    }
+
+    public function update(tag $tag)
+    {
+        $tag->update([
+            'tag' => request('tag'),
+            'descripcion' => request('descripcion'),
+        ]);
+
+        return redirect()->route('listaTag');
+    }
+
+    public function eliminar(tag $tag)
+    {
+        return view('eliminarTag', [
+            'tag' => $tag
+        ]);
+    }
+
+    public function delete(tag $tag){
+        
+        $tag->delete();
+
+        return redirect()->route('listaTag');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 //Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/show/{id}','tagController@show');
+
 Route::get ('/create','tagController@vista');
 Route::post ('/create','tagController@create');
+
+Route::get ('/listaTag','tagController@read')->name('listaTag');
+
+Route::get('/editar/{tag}','tagController@edit')->name('editar');
+Route::patch('/update/{tag}','tagController@update')->name('update');
+
+Route::get('/eliminar/{tag}','tagController@eliminar')->name('eliminar');
+Route::delete('/delete/{tag}', 'tagController@delete')->name('delete');
