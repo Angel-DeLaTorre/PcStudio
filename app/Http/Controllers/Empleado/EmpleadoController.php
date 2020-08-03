1a3ee3ffa29263cc50c5bf90a56783f4adafb42c
mario ornelas <mariodom987_@hotmail.com>
2020-08-03 03:07:08 -0500

    Modulo chatBot

--- app/Http/Controllers/Empleado/EmpleadoController.php
+++ app/Http/Controllers/Empleado/EmpleadoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Empleado;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class EmpleadoController extends Controller
 {
