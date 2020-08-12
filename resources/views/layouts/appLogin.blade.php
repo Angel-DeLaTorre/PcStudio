923910ebe75849fc56f0454ae0868c744681d3b5
Angel de la Torre <angel.delatorre.glez@gmail.com>
2020-08-11 22:58:14 -0500

    login

--- resources/views/layouts/appLogin.blade.php
+++ resources/views/layouts/appLogin.blade.php
@@ -106,7 +106,15 @@
 
  <body>
      <div id="app">
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">
+                <img src="{{ url('img/logo_h.png') }}" alt="" width="180">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
          
          <main class="py-4">
             @yield('content')
