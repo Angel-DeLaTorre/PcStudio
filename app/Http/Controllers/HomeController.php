1a83890385380a285fe939f356bdeb76626327c0
Omar Medrano <omar.medrano.ms@gmail.com>
2020-08-13 21:44:00 -0500

    Se finalizo el resumen de productos, usuarios y ventas para el admin o empleado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,49 +33,91 @@ class HomeController extends Controller
     {
 
         $saludo = $this->obtenerSaludo();
-        
+
         $nombreSaludo = $this->obtenerNombreUsuario();
-        
+
         $productosMasVendidos = $this->obtenerProductoMasVendido();
 
-        $cantidadUsuarios = $this->contarNuevosUsuariosUltimoMes();
+        $cantidadUsuarios = $this->contarNuevosUsuarios();
 
-        return view('home', ['saludo' => $saludo, 'nombreSaludo' => $nombreSaludo,
-         'cantidadUsuarios' => $cantidadUsuarios, 'productosMasVendidos' => $productosMasVendidos]);
+        $cantidadVentas = $this->obtenerCantidadVentas();
+
+        return view('home', [
+            'saludo' => $saludo, 'nombreSaludo' => $nombreSaludo,
+            'cantidadUsuarios' => $cantidadUsuarios, 'productosMasVendidos' => $productosMasVendidos,
+            'cantidadVentas' => $cantidadVentas
+        ]);
     }
+
+    private function obtenerCantidadVentas(){
+
+        $cantidadVentasActual = DB::table('detalleCompra')
+        ->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
+        ->whereMonth('compra.fechaCompra', '=', Carbon::now()->month)->whereYear('compra.fechaCompra', '=', Carbon::now()->year)->sum('detalleCompra.cantidad');
+
+        $cantidadVentasPasado = DB::table('detalleCompra')
+        ->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
+        ->whereMonth('compra.fechaCompra', '=', Carbon::now()->subMonth()->format('m'))->sum('detalleCompra.cantidad');
+
+        $cantidadVentas = array();
+
+        if($cantidadVentasActual){
+
+            array_push($cantidadVentas, $cantidadVentasActual);
+            
+        } else{
+            
+            array_push($cantidadVentas, 0);
+
+        }
+
+        if($cantidadVentasPasado){
+
+            array_push($cantidadVentas, $cantidadVentasPasado);
+            
+        } else{
+            
+            array_push($cantidadVentas, 0);
+
+        }
+
+        return $cantidadVentas;
+
+    }
+
 
 
     //Consulta los nombres de los productos más vendidos durante el 
     //mes actual y el mes anterior
     private function obtenerProductoMasVendido()
-    {   
+    {
 
-        $tituloProductoActual = DB::table('producto')->select('titulo')->where('idProducto', function($query){
-            $query-> select('idProducto')->from('detalleCompra')->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
-            ->whereMonth('compra.fechaCompra', '=', Carbon::now()->month)->whereYear('compra.fechaCompra', '=', Carbon::now()->year)
-            ->orderByDesc('cantidad')->limit(1);
+        $tituloProductoActual = DB::table('producto')->select('titulo')->where('idProducto', function ($query) {
+            $query->select('idProducto')->from('detalleCompra')->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
+                ->whereMonth('compra.fechaCompra', '=', Carbon::now()->month)->whereYear('compra.fechaCompra', '=', Carbon::now()->year)
+                ->orderByDesc('cantidad')->limit(1);
         })->get();
 
-        $tituloProductoAnterior = DB::table('producto')->select('titulo')->where('idProducto', function($query){
-            $query-> select('idProducto')->from('detalleCompra')->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
-            ->whereMonth('compra.fechaCompra', '=', Carbon::now()->subMonth()->format('m'))->orderByDesc('cantidad')->limit(1);
+        $tituloProductoAnterior = DB::table('producto')->select('titulo')->where('idProducto', function ($query) {
+            $query->select('idProducto')->from('detalleCompra')->join('compra', 'detalleCompra.idCompra', '=', 'compra.idCompra')
+                ->whereMonth('compra.fechaCompra', '=', Carbon::now()->subMonth()->format('m'))->orderByDesc('cantidad')->limit(1);
         })->get();
 
-        
+
         $productos = array();
 
 
 
-        if($tituloProductoActual){
-            array_push($productos,$tituloProductoActual[0]->titulo);
-        } else{
-            array_push($productos,"Sin productos populares este mes");
+        if ($tituloProductoActual) {
+            array_push($productos, $tituloProductoActual[0]->titulo);
+        } else {
+            array_push($productos, "Sin productos populares este mes");
         }
 
-        if($tituloProductoAnterior){
-            array_push($productos,$tituloProductoAnterior[0]->titulo);
-        } else{
-            array_push($productos,"Sin productos populares el mes anterior");
+        if ($tituloProductoAnterior) {
+            array_push($productos, $tituloProductoAnterior[0]->titulo);
+        } else {
+            array_push($productos, "Sin productos populares el mes anterior");
         }
 
         return $productos;
@@ -113,7 +155,7 @@ class HomeController extends Controller
             $saludo = 'Buenos días';
         } else if ($hora >= 12  && $hora < 19) {
             $saludo = 'Buenas tardes';
-        } else if ($hora >= 19 && $hora < 1) {
+        } else if ($hora >= 19 && $hora < 24) {
             $saludo = 'Buenas noches';
         } else {
             $saludo = 'Buen día';
@@ -123,15 +165,28 @@ class HomeController extends Controller
     }
 
     //Realiza el conteo de los usuarios de tipo cliente registrados durante el último mes
-    private function contarNuevosUsuariosUltimoMes()
+    private function contarNuevosUsuarios()
     {
 
-        $data = DB::table('users')->where('idRol', '=', 1)->whereMonth('created_at', '=', Carbon::now()->month)->whereYear('created_at', '=', Carbon::now()->year)->count();
+        $cantidadUltimoMes = DB::table('users')->where('idRol', '=', 1)->whereMonth('created_at', '=', Carbon::now()->month)->whereYear('created_at', '=', Carbon::now()->year)->count();
 
-        if ($data) {
-            return $data;
+        $cantidadMesPasado = DB::table('users')->where('idRol', '=', 1)->whereMonth('created_at', '=', Carbon::now()->subMonth()->format('m'))->count();
+
+        $nuevosUsuarios = array();
+
+        if ($cantidadUltimoMes) {
+            array_push($nuevosUsuarios, $cantidadUltimoMes);
         } else {
-            return 0;
+            array_push($nuevosUsuarios, 0);
         }
+
+        if ($cantidadMesPasado) {
+            array_push($nuevosUsuarios, $cantidadMesPasado);
+        } else {
+            array_push($nuevosUsuarios, 0);
+        }
+
+
+        return $nuevosUsuarios;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,19 +22,24 @@
                     <div class="row">
                         <div class="col">
                             <h5 class="card-title text-uppercase text-muted mb-0">MÁS VENDIDO ESTE MES</h5>
-                            <h6><span class="h2 font-weight-bold mb-0">{{ $productosMasVendidos[0] }}</span></h6>
+                            <h4><span class="font-weight-bold mb-0 mt-0">{{ $productosMasVendidos[0] }}</span></h4>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-info text-white
-                    rounded-circle shadow">
+                                                rounded-circle shadow">
                                 <i class="ni ni-active-40"></i>
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-nowrap">MÁS VENDIDO DEL MES PASADO</span><br />
-                        <b> <span class="text-info mr-2"><i class="fa fa-arrow-up"></i>
-                                {{ $productosMasVendidos[1] }}</span></b>
+                    <p class="mt-0 mb-0 text-sm">
+                    <h5 class="card-title text-uppercase text-muted mb-1">MÁS VENDIDO DEL MES ANTERIOR</h5>
+                    <b>
+                        <h4>
+                            <span class="font-weight-bold text-info mb-0 mt-0">
+                                {{ $productosMasVendidos[1] }}
+                            </span>
+                        </h4>
+                    </b>
                     </p>
                 </div>
             </div>
@@ -45,22 +50,31 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">
-                                Clientes nuevos este mes
-                            </h5>
-                            <span class="h2 font-weight-bold mb-0">{{ $cantidadUsuarios }}</span>
+                            <h5 class="card-title text-uppercase text-muted mb-2">CLIENTES NUEVOS ESTE MES</h5>
+
+                            <span class="h2 font-weight-bold mb-0">
+                                {{ $cantidadUsuarios[0] }}
+                            </span>
+                            <span class="h6 text-uppercase mb-0">USUARIOS REGISTRADOS</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-orange
-                    text-white rounded-circle shadow">
+                                                text-white rounded-circle shadow">
                                 <i class="ni ni-chart-pie-35"></i>
                             </div>
                         </div>
                     </div>
                     <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i>
-                            3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
+                    <h5 class="card-title text-uppercase text-muted mb-2">
+
+                        CLIENTES NUEVOS DEL MES ANTERIOR
+                    </h5>
+                    <b>
+                        <span class="text-warning mr-2"><i class="fa fa-arrow-up"></i>
+                            {{ $cantidadUsuarios[1] }}
+                        </span>
+                        <span class="h6 text-uppercase mb-0">USUARIOS REGISTRADOS</span>
+                    </b>
                     </p>
                 </div>
             </div>
@@ -71,25 +85,35 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Ventas del mes</h5>
-                            <span class="h2 font-weight-bold mb-0">924</span>
+                            <h5 class="card-title text-uppercase text-muted mb-2">VENTAS DURANTE ESTE MES</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $cantidadVentas[0] }} </span>
+                            <span class="h6 text-uppercase mb-0">PRODUCTOS VENDIDOS</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-green text-white
-                    rounded-circle shadow">
+                                                rounded-circle shadow">
                                 <i class="ni ni-money-coins"></i>
                             </div>
                         </div>
                     </div>
                     <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i>
-                            3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
+                    <h5 class="card-title text-uppercase text-muted mb-2">VENTAS DURANTE EL MES ANTERIOR</h5>
+                    <b>
+                        <span class="text-success mr-3"><i class="fa fa-arrow-up"></i>
+                            {{ $cantidadVentas[1] }}
+                        </span>
+                        <span class="h6 text-uppercase mb-0">PRODUCTOS VENDIDOS</span>
+                    </b>
+
                     </p>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
     </div>
     </div>
     </div>
